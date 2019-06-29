3b264d6b3b187355839224a9a05d641ddbab7a4f
Chelsea Springer <c1plus1equals10@gmail.com>
2019-06-29 19:23:39 -0400

    get user repos

--- screener/src/app/app.component.ts
+++ screener/src/app/app.component.ts
@@ -10,17 +10,32 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title = 'Screener';
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  readonly github_url = 'https://api.github.com';
+  readonly clientID = 'cd50000a794e64f34cb7';
+  readonly clientSecret = '4dd1342dc309538c4802bf549ed8a11ab7566a58';
   constructor(private http: HttpClient) {}
 
   posts: Observable<any>;
-  getPosts() {
-    console.log('Called GetPosts')
+  message: Observable<any>;
+  getDummyPosts() {
+    console.log('Called GetDummyPosts')
     const headers = new HttpHeaders().set('Authorization', 'auth-token');
     const params = new HttpParams().set('userId', '1');
     this.posts = this.http.get(this.ROOT_URL + '/posts', {headers}, {params})
   }
 
-  createPost() {
+  getGithubPosts() {
+    console.log('Called GetGithubPosts')
+    const headers = new HttpHeaders().set('Authorization', this.oauthtoken);
+    this.message = this.http.get(this.github_url + '/users/8bitOctoCat/repos?client_id ='+
+    this.clientID+'&client_secret='+this.clientSecret)
+    this.message.subscribe(res => {
+      console.log(res)
+      this.message = res;
+    })
+  }
+
+  createDummyPost() {
     const data = {
       id: null,
       userId: 23,
