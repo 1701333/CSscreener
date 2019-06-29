58afea2a1eb24994c2ce334f306c6ab641ec5e9b
Chelsea Springer <c1plus1equals10@gmail.com>
2019-06-29 18:38:34 -0400

    initial

--- screener/src/app/app.component.ts
+++ screener/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'screener';
+  title = 'Screener';
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  constructor(private http: HttpClient) {}
+
+  posts: Observable<any>;
+  getPosts() {
+    console.log('Called GetPosts')
+    this.posts = this.http.get(this.ROOT_URL + '/posts')
+  }
+
+  createPost() {
+    const data = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World!'
+    }
+
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data)
+  }
 }
